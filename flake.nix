733ceccbfc70345610573334612e6589688d4f5d
Mikael Voss <mvs@nyantec.com>
2024-07-30 14:24:13 +0200

    Add HomeManager module

--- flake.nix
+++ flake.nix
@@ -1,19 +1,30 @@
 {
   description = "Custom locale";
-  outputs = { self, ... }: {
-    nixosModules.default = { config, lib, pkgs, ... }: {
-      i18n.defaultLocale = lib.mkDefault "en_EU.UTF-8";
-
-      i18n.glibcLocales = (pkgs.glibcLocales.overrideAttrs (base: {
+  outputs = { self, nixpkgs, ... }:
+  let
+    patchLocales = pkgs: ovr:
+      (pkgs.glibcLocales.overrideAttrs (base: {
         postPatch = base.postPatch + ''
-          cp ${lib.escapeShellArg ./en_EU} localedata/locales/en_EU
+          cp ${./en_EU} localedata/locales/en_EU
           echo 'en_EU.UTF-8/UTF-8 \' >>localedata/SUPPORTED
         '';
-      })).override {
+      })).override ovr;
+  in {
+    nixosModules.default = { config, lib, pkgs, ... }: {
+      i18n.defaultLocale = lib.mkDefault "en_EU.UTF-8";
+      i18n.glibcLocales = patchLocales pkgs {
         allLocales = builtins.any (x: x == "all")
           config.i18n.supportedLocales;
 
         locales = config.i18n.supportedLocales;
+      };
+    };
+
+    homeModules.default = { lib, pkgs, ... }: {
+      home.sessionVariables.LANG = lib.mkDefault "en_EU.UTF-8";
+      i18n.glibcLocales = patchLocales pkgs {
+        allLocales = false;
+        locales = [ "en_EU.UTF-8/UTF-8" ];
       };
     };
   };
